d2ae95e44e0a785f44b14d4e6d7cf0e5514a8e54
hamzaomaro <Hamza.Omar@microsoft.com>
2023-09-28 16:14:50 +0300

    removed all extra libs to optimize memory

--- PILRepro/__init__.py
+++ PILRepro/__init__.py
@@ -63,9 +63,6 @@ import azure.functions as func
 
 from PIL import Image
 
-from django.shortcuts import render
-
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
